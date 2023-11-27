10470e7dfb3abd2a1e7b47a9f371754f312bbb53
Renan Almeida <renanvictoralmeidasilva@gmail.com>
2023-11-27 10:39:03 -0300

    feature/criando arquivo tela emprestimo

--- Bookify.api/Bookify/src/bookify/Controller/RealizarEmprestimoController.java
+++ Bookify.api/Bookify/src/bookify/Controller/RealizarEmprestimoController.java
@@ -1,27 +1,71 @@
 package bookify.Controller;
 
-import bookify.Treinando;
 import java.io.IOException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 public class RealizarEmprestimoController {
 
     private TelasController tela = new TelasController();
     
     @FXML
-    protected void alunoMenu() throws IOException{
+    private DatePicker LivDateDevolucao;
+
+    @FXML
+    private DatePicker LivDateInicio;
+
+    @FXML
+    private TextField LivTextAutor;
+
+    @FXML
+    private TextField LivTextCod;
+
+    @FXML
+    private TextField LivTextEditora;
+
+    @FXML
+    private TextField LivTextExemplar;
+
+    @FXML
+    private TextField LivTextMatricula;
+
+    @FXML
+    private TextField LivTextNome;
+
+    @FXML
+    private TextField LivTextObservacao;
+
+    @FXML
+    private TextField LivTextTelefone;
+
+    @FXML
+    private TextField LivTextTitulo;
+
+    @FXML
+    void alunoMenu(ActionEvent event) throws IOException {
         tela.switchScreen(1);
     }
+
     @FXML
-    protected void professorMenu() throws IOException{
-        tela.switchScreen(2);
-    }
-    @FXML
-    protected void homeMenu() throws IOException{
+    void homeMenu(MouseEvent event) throws IOException {
         tela.switchScreen(4);
     }
+
     @FXML
-    protected void livroMenu() throws IOException{
+    void livroMenu(ActionEvent event) throws IOException {
         tela.switchScreen(7);
     }
+
+    @FXML
+    void professorMenu(ActionEvent event) throws IOException {
+        tela.switchScreen(2);
+    }
+    
+    
+
 }
